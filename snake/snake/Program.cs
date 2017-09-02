dba727c657a7e0e03af922200a1f16164fec4d40
stiklov <stiklov@yandex.com>
2017-09-02 04:43:45 +0300

    Начало

---
+++ snake/snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace snake
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
